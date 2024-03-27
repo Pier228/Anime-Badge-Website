1c8f887c720fd59b629b887eab8d34617971cfaa
Pier228 <artem.buxanenko@gmail.com>
2024-03-27 21:16:59 +0200

    Added container, icon, submit button for settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next/types";
 import styles from "../../styles/settings/settings.module.scss";
 import btn_styles from "../../styles/buttons/buttons.module.scss";
 import Link from "next/link";
+import badgeStyle from "@/styles/mainBadge/main-badge.module.scss";
+import NickName from "@/components/MainBadge/header/NickName";
+import Image from "next/image";
+import buttonStyle from "@/styles/buttons/buttons.module.scss";
 
 export const metadata: Metadata = {
     title: "Settings",
@@ -12,9 +16,36 @@ const Settings = () => {
     return (
         <>
             <Link href="/" className={btn_styles.left_btn}>
+                <Image
+                    src="/images/home.png"
+                    width={20}
+                    height={20}
+                    alt="settings"
+                />
                 Back to main
             </Link>
-            <h1 className={styles.alert_text}>In developing</h1>
+            <div className={badgeStyle.container_35}>
+                <Image
+                    src="/images/katana.png"
+                    width={200}
+                    height={200}
+                    alt="Katana"
+                    className={badgeStyle.katana_img_top}
+                />
+                <header className={styles.header}>
+                    <NickName name="Settings" />
+                </header>
+                <main className={styles.main}>
+                    <p>Character: </p>
+                    <p>NickName:</p>
+                    <p>Bottom text:</p>
+                </main>
+                <footer className={styles.footer}>
+                    <button className={buttonStyle.submit_btn}>
+                        Apply changes
+                    </button>
+                </footer>
+            </div>
         </>
     );
 };
