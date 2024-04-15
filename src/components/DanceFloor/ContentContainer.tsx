9eeb45c1e91bea4738e16c4e0b7e207b8f8c04c0
Pier228 <artem.buxanenko@gmail.com>
2024-04-15 15:40:52 +0300

    Added caption for images on dance floor settings

--- src/components/DanceFloor/ContentContainer.tsx
+++ src/components/DanceFloor/ContentContainer.tsx
@@ -29,6 +29,7 @@ const ContentContainer = (props: IContentContainerFull) => {
                 alt={props.name}
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
+            <p className={styles.caption}>{props.name}</p>
         </label>
     );
 };
